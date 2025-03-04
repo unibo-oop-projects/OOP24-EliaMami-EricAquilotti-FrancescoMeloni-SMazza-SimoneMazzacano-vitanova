e75602f0f1d0c697ddb7f5ca5df4e16512dbebef
Eric-Aquilotti <eric.aquilotti2@studio.unibo.it>
2025-03-04 20:53:00 +0100

    Fixed window too small at the start

--- src/main/java/it/unibo/view/screen/ScreenImpl.java
+++ src/main/java/it/unibo/view/screen/ScreenImpl.java
@@ -67,7 +67,6 @@ public final class ScreenImpl extends JPanel implements Screen {
         window.addKeyListener(inputHandler);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        window.pack();
         initializeBuffer();
     }
 
