54f83d5e994fd008f816ba60a749761538545bd0
Eric-Aquilotti <eric.aquilotti2@studio.unibo.it>
2025-03-27 22:56:15 +0100

    Added human type so that it's easier to have the human names

---
+++ src/main/java/it/unibo/view/sprite/HumanType.java
@@ -0,0 +1,12 @@
+package it.unibo.view.sprite;
+
+/**
+ * Enum to easily get the name of the humans.
+ */
+public enum HumanType {
+    /**
+     * The human types.
+     */
+    PLAYER, MALE, FEMALE;
+}
+
