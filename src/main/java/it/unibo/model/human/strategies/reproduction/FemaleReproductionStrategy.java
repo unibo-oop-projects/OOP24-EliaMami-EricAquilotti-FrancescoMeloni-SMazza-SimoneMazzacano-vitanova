1d9e0dbec722257aed8d407c2bb32b6d97267b42
Eric-Aquilotti <eric.aquilotti2@studio.unibo.it>
2025-03-28 20:53:00 +0100

    Added abstract class for reproduction strategies to remove duplicated code

--- src/main/java/it/unibo/model/human/strategies/reproduction/FemaleReproductionStrategy.java
+++ src/main/java/it/unibo/model/human/strategies/reproduction/FemaleReproductionStrategy.java
@@ -1,7 +1,5 @@
 package it.unibo.model.human.strategies.reproduction;
 
-import it.unibo.common.Circle;
-import it.unibo.common.CircleImpl;
 import it.unibo.common.Position;
 import it.unibo.model.human.Human;
 import it.unibo.view.sprite.HumanType;
@@ -11,13 +9,8 @@ import it.unibo.view.sprite.HumanType;
  * the player and has a cooldown for new reproductions. It also moves the center
  * of the reproduction area based on the human position.
  */
-public final class FemaleReproductionStrategy implements ReproductionStrategy {
-    // I want the center to be around the legs of the human.
-    private static final int CIRCLE_X_OFFSET = 16;
-    private static final int CIRCLE_Y_OFFSET = 24;
-    private static final int CIRCLE_RADIOUS = 12;
+public final class FemaleReproductionStrategy extends AbstractReproductionStrategy {
     private static final int REPRODUCTION_COOLDOWN = 100;
-    private final Circle reproductionArea;
     private boolean canReproduce = true;
     private int reproductionCounter;
 
@@ -27,16 +20,12 @@ public final class FemaleReproductionStrategy implements ReproductionStrategy {
      * the reproduction area.
      */
     public FemaleReproductionStrategy(final Position startingPosition) {
-        reproductionArea = new CircleImpl(
-            startingPosition.x() + CIRCLE_X_OFFSET,
-            startingPosition.y() + CIRCLE_Y_OFFSET,
-            CIRCLE_RADIOUS
-        );
+        super(startingPosition);
     }
 
     @Override
     public void update(final Position humanPosition) {
-        centerReproductionArea(humanPosition);
+        super.update(humanPosition);
         if (!canReproduce) {
             reproductionCounter++;
             if (reproductionCounter > REPRODUCTION_COOLDOWN) {
@@ -49,19 +38,10 @@ public final class FemaleReproductionStrategy implements ReproductionStrategy {
     @Override
     public boolean collide(final Human other) {
         if ((other.getType() == HumanType.MALE || other.getType() == HumanType.PLAYER)
-            && canReproduce && reproductionArea.intersects(other.reproductionArea())) {
+            && canReproduce && getReproductionArea().intersects(other.reproductionArea())) {
             canReproduce = false;
             return true;
         }
         return false;
-    }
-
-    @Override
-    public Circle getReproductionArea() {
-        return new CircleImpl(reproductionArea);
-    }
-
-    private void centerReproductionArea(final Position humanPosition) {
-        reproductionArea.setCenter(humanPosition.x() + CIRCLE_X_OFFSET, humanPosition.y() + CIRCLE_Y_OFFSET);
     }
 }
