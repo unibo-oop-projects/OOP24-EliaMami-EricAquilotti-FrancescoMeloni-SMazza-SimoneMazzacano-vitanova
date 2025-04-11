74d968fc518d74056c8a8e52f817abc6ca048c32
Francesco Meloni <francesco.meloni6@studio.unibo.it>
2025-04-11 15:32:14 +0200

    Now the player cannot spawn in a non walkable tile

--- src/main/java/it/unibo/common/Position.java
+++ src/main/java/it/unibo/common/Position.java
@@ -1,9 +1,31 @@
 package it.unibo.common;
 
+import java.util.Random;
+
+import it.unibo.model.chapter.map.Map;
+import it.unibo.view.screen.ScreenImpl;
+
 /**
  * 
  * @param x the position on the x axes.
  * @param y the position on the y axes.
  */
 public record Position(double x, double y) {
+
+    /**
+     * Returns a walkable position given a map.
+     * @param map the map we want to have a walkable position.
+     * @return a walkable position within the map.
+     */
+    public static Position getRandomWalkablePosition(final Map map) {
+        final Random rand = new Random();
+        Position p = new Position(0, 0);
+        while (!map.getTileFromPixel(p.x, p.y).isWalkable()) {
+            p = new Position(
+                (double) Math.abs(rand.nextInt(0, map.getColoumns())) * ScreenImpl.TILE_SIZE,
+                (double) Math.abs(rand.nextInt(0, map.getRows())) * ScreenImpl.TILE_SIZE
+            );
+        }
+        return p;
+    }
 }
