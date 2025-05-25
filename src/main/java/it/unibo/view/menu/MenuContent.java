768f2a616939d5ab395f6303a1bcbb4a91fbce74
simone-mazzacano <simone.mazzacano2@studio.unibo.it>
2025-05-25 19:10:46 +0200

    Refactor MenuContent to be coherent with static factory method

--- src/main/java/it/unibo/view/menu/MenuContent.java
+++ src/main/java/it/unibo/view/menu/MenuContent.java
@@ -6,27 +6,70 @@ import java.util.Optional;
 
 /**
  * Class that represents the content of a menu.
- * @param title the title of the menu
- * @param subtitle the subtitle of the menu
- * @param options the list of options in the menu
- * @param textSize the size of the text in the menu
  */
-public record MenuContent(String title, String subtitle, List<String> options, Optional<Integer> textSize) {
-    /**
-     * Constructor for the MenuContent class.
-     */
-    public MenuContent {
-        options = Collections.unmodifiableList(options);
-    }
+public final class MenuContent {
+    private final String title;
+    private final String subtitle;
+    private final List<String> options;
+    private final Optional<Integer> textSize;
 
     /**
      * Constructor for the MenuContent class.
      * @param title the title of the menu
      * @param subtitle the subtitle of the menu
      * @param options the list of options in the menu
+     * @param textSize the size of the text in the menu, can be empty
      */
-    public MenuContent(final String title, final String subtitle, final List<String> options) {
+    private MenuContent(final String title, final String subtitle, final List<String> options,
+                        final Optional<Integer> textSize) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.options = Collections.unmodifiableList(options);
+        this.textSize = textSize;
+    }
+
+    private MenuContent(final String title, final String subtitle, final List<String> options) {
         this(title, subtitle, options, Optional.empty());
+    }
+
+    /**
+     * Gets the title of the menu.
+     * @return the title of the menu
+     */
+    public String title() {
+        return title;
+    }
+    /**
+     * Gets the subtitle of the menu.
+     * @return the subtitle of the menu
+     */
+    public String subtitle() {
+        return subtitle;
+    }
+    /**
+     * Gets the list of options in the menu.
+     * @return the list of options in the menu
+     */
+    public List<String> options() {
+        return options;
+    }
+    /**
+     * Gets the text size of the menu.
+     * @return an Optional containing the text size if it is set, otherwise an empty Optional
+     */
+    public Optional<Integer> textSize() {
+        return textSize;
+    }
+
+    /**
+    * Creates a new MenuContent object with the specified title, subtitle, and options.
+     * @param title the title of the menu
+     * @param subtitle the subtitle of the menu
+     * @param options the list of options in the menu
+     * @return a new MenuContent object with the specified title, subtitle, and options
+     */
+    public static MenuContent of(final String title, final String subtitle, final List<String> options) {
+        return new MenuContent(title, subtitle, options, Optional.empty());
     }
 
     /**
