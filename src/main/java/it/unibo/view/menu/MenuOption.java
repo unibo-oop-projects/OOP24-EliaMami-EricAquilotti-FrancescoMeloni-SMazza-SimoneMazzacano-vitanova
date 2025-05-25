4fd0c3e5fc3e8daea357921ba378bad1a7cbce03
simone-mazzacano <simone.mazzacano2@studio.unibo.it>
2025-05-25 14:16:20 +0200

    Refactor MenuOption to be static factory

--- src/main/java/it/unibo/view/menu/MenuOption.java
+++ src/main/java/it/unibo/view/menu/MenuOption.java
@@ -7,29 +7,51 @@ import it.unibo.controller.Game;
 import it.unibo.controller.InputHandler;
 
 /**
- * Class that handles the menu options, wraps the description and the action to be performed.
- * @param desc the description of the menu option
- * @param action the action to be performed when the option is selected by the user
+ * Static factory class that handles the menu options, wraps the description and the action to be performed.
  */
-public record MenuOption(Supplier<String> desc, Consumer<Game> action) {
+public final class MenuOption {
+    private final Supplier<String> desc;
+    private final Consumer<Game> action;
+    private MenuOption(final Supplier<String> desc, final Consumer<Game> action) {
+        this.desc = desc;
+        this.action = action;
+    }
+    /**
+     * Gets the description of the menu option.
+     * @return a Supplier that provides the description of the menu option
+     */
+    public Supplier<String> desc() {
+        return desc;
+    }
+    /**
+     * Gets the action to be performed when the menu option is selected.
+     * @return a Consumer that takes a Game object and performs the action
+     */
+    public Consumer<Game> action() {
+        return action;
+    }
     /**
      * Static factory method for the MenuOption class.
      * @param desc the description of the menu option
      * @param action the action to be performed when the option is selected by the user
+     * @return a MenuOption instance with the given description and action
      */
     public static MenuOption of(final String desc, final Consumer<Game> action) {
         return new MenuOption(() -> desc, action);
     }
 
     /**
-     * 
-     * @return
+     * Static factory method for the MenuOption class.
+     * @param desc the description of the menu option
+     * @param action the action to be performed when the option is selected by the user
+     * @return a MenuOption instance with the given description and action
      */
     public static MenuOption of(final Supplier<String> desc, final Consumer<Game> action) {
         return new MenuOption(desc, action);
     }
 
     /**
+     * Static factory method for a MenuOption to quit the game.
      * @return a MenuOption to quit the game
      */
     public static MenuOption quit() {
