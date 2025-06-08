b1a998a4b2183b99ecc6f8be4e07863dcfb224b0
Elia-Mami <elia.mami@studio.unibo.it>
2025-06-08 21:16:04 +0200

    Removed old javadoc

--- src/main/java/it/unibo/model/effect/EffectFactory.java
+++ src/main/java/it/unibo/model/effect/EffectFactory.java
@@ -15,28 +15,4 @@ public interface EffectFactory {
      * @return an effect that describe how to modify the chosen stat.
      */
     Effect createEffect(EffectType type, Duration duration, double multiplyValue);
-
-    // /**
-    //  * Method that return a new reproduction range effect.
-    //  * @param duration the duration
-    //  * @param multiplyValue the value to multiply the parameter with
-    //  * @return an effect that describe how to modify the radius range.
-    //  */
-    // Effect reproductionRangeEffect(Duration duration, double multiplyValue);
-
-    // /**
-    //  * Method that return a new fertility effect.
-    //  * @param duration the duration
-    //  * @param multiplyValue the value to multiply the parameter with
-    //  * @return an effect that describe how to modify the fertility.
-    //  */
-    // Effect fertilityEffect(Duration duration, double multiplyValue);
-
-    // /**
-    //  * Method that return a sickness resistence effect.
-    //  * @param duration the duration
-    //  * @param multiplyValue the value to multiply the parameter with
-    //  * @return an effect that describe how to modify the sickness resistence.
-    //  */
-    // Effect sicknessResistenceEffect(Duration duration, double multiplyValue);
 }
